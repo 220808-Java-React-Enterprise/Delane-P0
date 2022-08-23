019f61be93abc2fb2bbaf576855f3dce71cd8e09
del4235 <110851058+del4235@users.noreply.github.com>
2022-08-23 18:29:47 -0400

    Cleaning github folder.

--- ProjectFolder/MyStore/src/main/java/com/revature/BubbleCraft/services/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.revature.BubbleCraft.services;
-
-import com.revature.BubbleCraft.daos.UserDAO;
-
-public class CustomerService extends UserService{
-    public CustomerService(UserDAO userDAO) {
-        super(userDAO);
-    }
-}
